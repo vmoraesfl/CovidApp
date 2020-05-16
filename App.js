64f47655a56c3819d72426afe736eb540f040bc8
Moraes <vmoraes.fl@gmail.com>
2020-05-16 20:48:57 -0300

    vai?

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ma que Diabo!</Text>
+      <Text>Third time is the charm!</Text>
     </View>
   );
 }
